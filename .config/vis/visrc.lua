cc259521af59aaca8714dc98d20e1858b37e18d3
Lucas Ieks <lieks124@gmail.com>
2022-02-09 11:20:36 -0300

    Add vis link following with gx

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -2,7 +2,23 @@ require 'vis'
 require 'plugins.vis-cursors'
 
 vis.events.subscribe(vis.events.INIT, function()
-	-- Your global configuration options
+	vis:map(vis.modes.NORMAL, "gx", function()
+		local line = vis.win.selection.line
+		local pos = vis.win.selection.col
+		local str = vis.win.file.lines[line]
+		local len_str = #str
+
+		local URLchars = '[^a-zA-Z0-9%?._=+;&/:@#-]'
+		local to = str:find(URLchars, pos)
+		if to == nil then to = len_str else to = to - 1 end
+		local from = str:reverse():find(URLchars, len_str - pos + 1)
+		if from == nil then from = 1 else from = len_str - from + 2 end
+		local word = str:sub(from, to)
+
+		local cmd = "setsid xdg-open '" .. word:gsub("'", "'\''") .. "'"
+
+		os.execute(cmd)
+	end, "Open URL")
 end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
